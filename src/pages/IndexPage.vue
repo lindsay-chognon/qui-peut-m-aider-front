c422bef47adaf5e03e79ef6b93ebd33158f90217
lindsay <lindsaychognon@gmail.com>
2022-07-11 20:38:37 +0200

    Test récupération des données de l'api

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <ul>
+      <li v-for="prestation of prestations" :key="prestation.id">
+        {{ prestation.titre }}
+      </li>
+    </ul>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+  import axios from "axios";
+  export default {
+    name: "App",
+    data() {
+      return {
+        prestations: [],
+      };
+    },
+    async created() {
+      try {
+        const res = await axios.get(`http://localhost:8000/api/prestations`);
+        this.prestations = res.data['hydra:member'];
+        console.log(res.data['hydra:member'])
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  };
 </script>
