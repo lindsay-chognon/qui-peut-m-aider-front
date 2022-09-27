eaebdeeefac44a5e02d95ac33db8d74225437db0
lindsay <lindsaychognon@gmail.com>
2022-09-27 15:40:47 +0200

    Affichage du component CardsPrestation dans l'index

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,30 +1,50 @@
 <template>
-  <q-page class="flex flex-center">
-    <ul>
-      <li v-for="prestation of prestations" :key="prestation.id">
-        {{ prestation.titre }}
-      </li>
-    </ul>
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md ">
+    <CardsPrestations v-for="prestation of prestations"
+                      :key="prestation.id "
+                      :titre="prestation.titre"
+                      :description="prestation.description"
+                      :ville="prestation.ville"
+                      :taux_horaire="prestation.taux_horaire"
+    >
+
+    </CardsPrestations>
+  </div>
+
 </template>
 
 <script>
-  import axios from "axios";
-  export default {
-    name: "App",
-    data() {
-      return {
-        prestations: [],
-      };
-    },
-    async created() {
-      try {
-        const res = await axios.get(`http://localhost:8000/api/prestations`);
-        this.prestations = res.data['hydra:member'];
-        console.log(res.data['hydra:member'])
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  };
+import axios from "axios";
+import CardsPrestations from "components/CardsPrestations";
+export default {
+  name: "App",
+  components: {
+    CardsPrestations
+  },
+  data() {
+    return {
+      prestations: [],
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:8001/api/prestations`);
+      this.prestations = res.data['hydra:member'];
+      console.log(res.data['hydra:member'])
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
+
+
+
+
+
