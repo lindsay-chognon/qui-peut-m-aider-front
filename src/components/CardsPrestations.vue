45597fdbeb5140fd7c677a2926362669fa227546
lindsay <lindsaychognon@gmail.com>
2022-09-27 15:41:44 +0200

    Component CardsPrestation avec les props + test sur le passage d'un id dans un bouton

---
+++ src/components/CardsPrestations.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-card dark bordered class="bg-grey-9 my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      <q-card-section>
+        <div class="text-h6">{{ titre }}</div>
+        <div class="text-subtitle2">{{ ville }}</div>
+        <div class="text-subtitle2">{{ taux_horaire }} € </div>
+      </q-card-section>
+
+      <q-separator dark />
+
+      <q-card-actions>
+        <router-link :to="{ name: 'prestation', params: { id: 61 }}"><q-btn flat>Action 1</q-btn></router-link>
+      </q-card-actions>
+
+    </q-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'titre',
+    'ville',
+    'taux_horaire',
+    'description',
+    'action',
+  ]
+}
+</script>
