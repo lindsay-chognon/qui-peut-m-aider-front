b855248ea8bc65717f53d9223db70d37d1d90838
lindsay <lindsaychognon@gmail.com>
2022-09-29 16:03:58 +0200

    tests heroku, ajout d'un bouton action

--- src/components/CardsPrestations.vue
+++ src/components/CardsPrestations.vue
@@ -11,6 +11,7 @@
 
       <q-card-actions>
         <router-link :to="{ name: 'prestation', params: { id: prestation.id }}"><q-btn @click="prestationDetails()" flat>Action 1</q-btn></router-link>
+        <router-link :to="{ name: 'prestation', params: { id: prestation.id }}"><q-btn @click="prestationDetails()" flat>Action 1</q-btn></router-link>
 
       </q-card-actions>
 
