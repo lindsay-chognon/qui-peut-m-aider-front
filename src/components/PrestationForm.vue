beb200d2d8dfec5ef9fbd4da9eee529dbf88c4f3
lindsay <lindsaychognon@gmail.com>
2022-10-04 20:08:29 +0200

    Premier jet form design

---
+++ src/components/PrestationForm.vue
@@ -0,0 +1,57 @@
+
+<template>
+
+    <div class="column q-pa-lg window-height window-width row justify-center items-center">
+      <div class="row">
+        <q-card square class="shadow-24" style="width:45em;">
+          <q-card-section class="bg-deep-purple-7">
+            <h4 class="text-h5 text-white q-my-md">Proposer une prestation</h4>
+            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+              <q-btn fab icon="add" color="purple-4" />
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-px-sm q-pt-xl">
+              <q-input class="q-pa-md" square clearable v-model="email" type="email" label="Email">
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+              <q-input class="q-pa-md" square clearable v-model="password" type="password" label="Password">
+              </q-input>
+                <q-input square clearable
+                  outlined
+                  v-model="text"
+                  filled
+                  type="textarea"
+                />
+            </q-form>
+          </q-card-section>
+
+          <q-card-actions class="q-px-lg">
+            <q-btn unelevated size="lg" color="purple-4" class="full-width text-white" label="Sign In" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-sm">
+            <p class="text-grey-6">Forgot your password?</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      email: '',
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
