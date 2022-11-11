6f33af5847076f7bf543f6a3074c421273ebf6a6
lindsay <lindsaychognon@gmail.com>
2022-11-11 20:44:45 +0100

    Ajout d'une page dashboard accessible seulement quand on est connecté

---
+++ src/pages/MonEspacePage.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <h1>Mon espace connecté</h1>
+</template>
+
+<script>
+import axios from "axios";
+import CardsPrestations from "components/CardsPrestations";
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
