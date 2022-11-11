e8105611bd77ffbcadf50fd4eccfa9fe07246abb
lindsay <lindsaychognon@gmail.com>
2022-11-11 20:43:49 +0100

    Ajout d'une fonction de vérification de l'expiration du token (à retester !)

--- src/_services/account.service.js
+++ src/_services/account.service.js
@@ -32,11 +32,30 @@ let getToken = () => {
   return jwt
 }
 
+let isExpired = () => {
+  let token = getToken()
+  if (token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+
+    return Date.now() / 1000 > JSON.parse(jsonPayload).exp;
+
+
+
+  }
+
+}
+
 // exporte tout dans accountService
 export const accountService = {
   login,
   logout,
   saveToken,
   isLogged,
-  getToken
+  getToken,
+  isExpired
 }
