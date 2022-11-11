b2ee1712651b7d58bb75d4ee6c164ddb68ca9bd5
lindsay <lindsaychognon@gmail.com>
2022-11-11 20:42:48 +0100

    Modification mise en page du code des routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,42 @@
+import { authGuard } from "src/_helpers/auth-guard";
+import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'prestations', name:'prestations', component: () => import('pages/PrestationsPage.vue') },
-      { path: 'prestation/:id', props: true, name:'prestation', component: () => import('pages/PrestationDetailsPage.vue') },
-      { path: 'prestation/add', name:'prestationAdd', component: () => import('pages/PrestationAddPage.vue') },
-      { path: 'login', name:'login', component: () => import('pages/LoginPage.vue') }
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue')
+      },
+      {
+        path: 'prestations',
+        name:'prestations',
+        component: () => import('pages/PrestationsPage.vue')
+      },
+      {
+        path: 'prestation/:id',
+        props: true,
+        name:'prestation',
+        component: () => import('pages/PrestationDetailsPage.vue')
+      },
+      {
+        path: 'prestation/add',
+        name:'prestationAdd',
+        component: () => import('pages/PrestationAddPage.vue')
+      },
+      {
+        path: 'login',
+        name:'login',
+        component: () => import('pages/LoginPage.vue'),
+      },
+      {
+        path: 'dashboard',
+        name:'dashboard',
+        component: () => import('pages/MonEspacePage.vue'),
+        beforeEnter: authGuard
+      }
     ]
   },
 
