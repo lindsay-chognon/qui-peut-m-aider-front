b08ab34dcf456ff86a8395045f275136cd32234c
lindsay <lindsaychognon@gmail.com>
2022-11-12 10:38:39 +0100

    Composant buttonAdd

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row justify-center">
+
+    <router-link :to="{ name: 'prestationAdd' }">
+      <q-btn
+        color="deep-orange-5"
+        icon="add"
+        label="Ajouter une prestation"
+        size="xl"
+      />
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonAdd"
+}
+</script>
+
+<style>
+a {
+  text-decoration: none !important;
+}
+</style>
