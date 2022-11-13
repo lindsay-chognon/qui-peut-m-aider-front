4660e7c09392500454b3f7f321650048d0f8bc72
lindsay <lindsaychognon@gmail.com>
2022-11-13 19:26:36 +0100

    Création d'un composant btn  "découvrir les prestations"

---
+++ src/components/ButtonDiscover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row justify-center">
+
+    <router-link :to="{ name: 'prestations' }">
+      <q-btn
+        color="deep-orange-5"
+        label="Découvrir les prestations"
+        size="md"
+        class="q-mt-lg"
+      />
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonDiscover"
+}
+</script>
+
+<style>
+a {
+  text-decoration: none !important;
+}
+</style>
