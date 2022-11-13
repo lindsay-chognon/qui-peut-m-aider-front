08d8369fa68f8f5dbf211accf304eb41adc7cb3e
lindsay <lindsaychognon@gmail.com>
2022-11-13 17:48:01 +0100

    Ajout d'un service de gestion de prestation avec axios

---
+++ src/_services/prestation.service.js
@@ -0,0 +1,34 @@
+import Axios from './caller.services'
+
+let getAllPrestations = () => {
+  return Axios.get('/api/prestations')
+}
+
+let getPrestation = (id) => {
+  return Axios.get('/api/prestations/'+id)
+}
+
+let updatePrestation = (prestation) => {
+  return Axios.patch('api/prestations/'+prestation.id, prestation)
+}
+
+let createVille = (ville) => {
+  return Axios.post('/api/villes', ville)
+}
+
+let createPrestation = (prestation) => {
+  return Axios.post('/api/prestations', prestation)
+}
+
+let deletePrestation = (id) => {
+  return Axios.delete('/api/prestations/'+id)
+}
+
+export const prestationService = {
+  getAllPrestations,
+  getPrestation,
+  updatePrestation,
+  createVille,
+  createPrestation,
+  deletePrestation
+}
