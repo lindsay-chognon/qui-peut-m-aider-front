a828aab3a9bcc3455af81d5eb8160e9f734ef2fb
lindsay <lindsaychognon@gmail.com>
2022-12-01 15:40:48 +0100

    Modification texte bannière

--- src/components/BookingBanner.vue
+++ src/components/BookingBanner.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md q-gutter-sm justify-center">
     <q-banner rounded class="bg-purple-8 text-white">
 
-      Vous devrez vous connecter pour réserver.
+      Vous devrez vous connecter pour effectuer une réservation.
 
       <template v-slot:action>
         <q-btn @click="$router.go(-1)" label="Retour"/>
